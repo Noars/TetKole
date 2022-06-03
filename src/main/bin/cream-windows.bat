c70372e5e98bab01d5967c2fb334e76a31a411b6
Noars <55838787+Noars@users.noreply.github.com>
2022-06-03 11:09:07 +0200

    Add auto release

--- src/main/bin/cream-windows.bat
+++ src/main/bin/cream-windows.bat
@@ -1,6 +1,7 @@
 @echo on
 
-SET JAVA_HOME=..\lib\jre
+SET JAVA_HOME=../lib/jre
+SET PATH=../lib/jre/bin;%PATH%
 
 java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar ../lib/translateAudioFiles.jar
 
