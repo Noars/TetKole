59bc1ba0302bf3e4cbd513af0d07d74b955af591
Noars <55838787+Noars@users.noreply.github.com>
2022-07-04 11:15:03 +0200

    Fix TètKole winodws installer

--- src/main/bin/tetkole-windows.bat
+++ src/main/bin/tetkole-windows.bat
@@ -1,8 +1,8 @@
 @echo on
 
-SET JAVA_HOME=..\lib\jre
-SET PATH=%JAVA_HOME\bin;%PATH%
+SET JAVA_HOME=../lib/jre
+SET PATH=../lib/jre/bin;%PATH%
 
-java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar ..\lib\TetKole.jar
+java -cp "../lib/*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar ../lib/TetKole.jar
 
 pause
